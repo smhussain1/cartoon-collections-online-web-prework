7190b17b7216e8e4223cdb1b070e2dcd56ae2abe
michael hussain <smhussain_1@yahoo.com>
2019-01-05 22:21:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-    if calls_long.any? do |call| call.length > 4
+    calls_long.any? do |call| call.length > 4
     #else calls_long.all? { |word| word.length < 4 } 
-  end   
+   
 end 
 end
  
